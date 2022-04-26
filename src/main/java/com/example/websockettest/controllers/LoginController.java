7e026c37f54b8cb59c170fa638c91322914926a7
Jason Rice <jrice@entrata.com>
2022-04-25 21:00:52 -0600

    CrossOrigin annotation.

--- src/main/java/com/example/websockettest/controllers/LoginController.java
+++ src/main/java/com/example/websockettest/controllers/LoginController.java
@@ -6,11 +6,13 @@ import com.example.websockettest.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "https://rpg-dnd-client.herokuapp.com/")
 public class LoginController {
     @Autowired
     private LoginService loginService;
